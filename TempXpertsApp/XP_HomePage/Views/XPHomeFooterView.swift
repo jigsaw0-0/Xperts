4a18fd8b98376f755c9351681fd3b67ec54a7a57
Sriram S <sriram.selvaraj@justdial.com>
2022-03-11 13:42:55 +0530

    Project name change

---
+++ TempXpertsApp/XP_HomePage/Views/XPHomeFooterView.swift
@@ -0,0 +1,20 @@
+//
+//  XPHomeFooterView.swift
+//  TempXpertsApp
+//
+//  Created by Sriram S on 10/03/22.
+//
+
+import UIKit
+
+class XPHomeFooterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
